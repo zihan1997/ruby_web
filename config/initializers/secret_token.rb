c2f3922e68eddc9b277ab76003803cc3fa9cf24d
BlueBug <zihan9701@gmail.com>
2018-06-28 20:21:22 +0800

    initialized non-fuc web

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '29ff5c4d560e4f5848bdf55988a762367ac2036aec5ef9d9b360b1209bf31d435af72dae798c3bedc46f11e086f7c29a6b1e665b64b406b7dfce409a89b6753b'
